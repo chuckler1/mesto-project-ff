902cae94e2b97df9998df57340add56afee1fdb3
Denis Kozlov <denisdesdoz@yandex.ru>
2023-10-08 19:36:36 +0300

    1.02

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,24 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(linkCard, titleCard, deleteCard) {
+function createCard(cardData, callback) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const recetButton = cardElement.querySelector('.card__delete-button');
   
-  cardElement.querySelector('.card__image').src = linkCard;
-  cardElement.querySelector('.card__title').textContent = titleCard;
-  recetButton.addEventListener('click', () => deleteCard(cardElement));
-  cardContainer.append(cardElement);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  recetButton.addEventListener('click', () => callback(cardElement));
+  return cardElement;
 }
 // @todo: Функция удаления карточки
 const deleteCardItem = function(elem) {
   elem.remove();
 }
+// Функция добалвения карточки в разметку
+function renderCard(el) {
+  cardContainer.append(createCard(el, deleteCardItem));
+}
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-  addCard(item.link, item.name, deleteCardItem);
+  renderCard(item);
 })
